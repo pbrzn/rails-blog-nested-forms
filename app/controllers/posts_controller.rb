a90342060c29b1f1cf354d9977c4ac6cbe5c475c
Patrick Brennan <pbrennanmusic@gmail.com>
2021-08-15 11:48:34 -0400

    commit tree a0b2f7ce768b45e850908201fc0c1a16a3b764a9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,6 +68,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      # binding.pry
+      params.require(:post).permit(:name, :content, :tag_ids => [], :tags_attributes => :name)
     end
 end
